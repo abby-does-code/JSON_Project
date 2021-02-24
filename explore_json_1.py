d0b097ad83950252dbb851acc673c30dfa22a70d
Abby Brown <abby_brown3@baylor.edu>
2021-02-24 15:10:57 -0600

    Working

--- explore_json_1.py
+++ explore_json_1.py
@@ -1,9 +1,18 @@
 import json
 
-infile = open("eq.data_1_day_m1.json", "r")
-outfile = open("readable_eq_data.json", "r")
+infile = open("eq_data_1_day_m1.json", "r")
+outfile = open("readable_eq_data.json", "w")
 
 eq_data = json.load(infile)
 
 json.dump(eq_data, outfile, indent=4)
 # Dump
+
+# print(eq_data["features"][0])
+# With this line of code, we get a dictionary back!
+
+# Version 2:
+# print(eq_data["features"][0]["properties"]["mag"])
+# This one takes us exactly to what we wanted--the magnited. But this is just the first one!
+
+mags = []
