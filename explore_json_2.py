50d6892bf784fca522acad68cd4f5e21380fdf17
Abby Brown <abby_brown3@baylor.edu>
2021-03-01 15:17:15 -0600

    In class work

--- explore_json_2.py
+++ explore_json_2.py
@@ -15,27 +15,36 @@ json.dump(eq_data, outfile, indent=4)
 # print(eq_data["features"][0]["properties"]["mag"])
 # This one takes us exactly to what we wanted--the magnited. But this is just the first one!
 
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts = [], [], [], []
 # Creates three lists; magnitudes, longitudes, and latitudes
 
 list_of_eqs = eq_data["features"]
+
 
 for eq in list_of_eqs:
     mag = eq["properties"]["mag"]
     lon = eq["geometry"]["coordinates"][0]
     # This calls the dictionary, the list (geometry) and the index number of #longitude in the list (0)
     lat = eq["geometry"]["coordinates"][1]
+    place = eq["properties"][
+        "place"
+    ]  # As we're going through the JSON file, we go throguh each equator one at a time and it's attaching the information. Each element and attributes are perfectly lined up.
+    # When we are creating data objects, we are giving it the lists and each of THOSE are lined up perfectly.
+    # If you were to change up the order, it would be all wrong.
 
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hover_texts.append(place)
 
+
+"""
 print(mags[:10])
 # Print out everything from first element to the tenth element
 # (0-9)
 print(lons[:10])
 print(lats[:10])
-
+"""
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
@@ -44,6 +53,7 @@ data = [
         "type": "scattergeo",
         "lon": lons,
         "lat": lats,
+        "text": hover_texts,
         "marker": {
             "size": [5 * mag for mag in mags],
             "color": mags,
