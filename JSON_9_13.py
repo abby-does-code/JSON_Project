d6a454a4d8606773f362a67ae1b057a92da10261
Abby Brown <abby_brown3@baylor.edu>
2021-03-03 20:06:47 -0600

    done

--- JSON_9_13.py
+++ JSON_9_13.py
@@ -7,26 +7,31 @@ outfile = open("readable_9_14.json", "w")
 fires_data = json.load(infile)
 
 json.dump(fires_data, outfile, indent=4)
+# Dump
 
-# Get data
+
 brightness_list, lons, lats = [], [], []
 
 for x in fires_data:
     bright_instance = x["brightness"]
     lon = x["longitude"]
+    # This calls the dictionary, the list (geometry) and the index number of #longitude in the list (0)
     lat = x["latitude"]
 
     brightness_list.append(bright_instance)
     lons.append(lon)
     lats.append(lat)
 
+
 fires_over_450 = [i for i in brightness_list if i > 450]
 print(fires_over_450[:10])
 print(lats[:10])
+# Print out everything from first element to the tenth element
 
-# Graph:
+
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline as offline
+
 
 data = [
     {
@@ -48,4 +53,4 @@ my_layout = Layout(title="Fires with Brightness over 450")
 
 fig = {"data": data, "layout": my_layout}
 
-offline.plot(fig, filename="fire_brightness.html")
+offline.plot(fig, filename="fire_brightness_2.html")
